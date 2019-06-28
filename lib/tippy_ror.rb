c4d1e101b106c12ae96227bff645558089fe110c
Sahidur Suman <suman5040@gmail.com>
2019-06-29 00:17:14 +0600

    Initial commit.

---
+++ lib/tippy_ror.rb
@@ -0,0 +1,6 @@
+require "tippy_ror/version"
+
+module TippyRor
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/tippy_ror_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe TippyRor do
+  it "has a version number" do
+    expect(TippyRor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
